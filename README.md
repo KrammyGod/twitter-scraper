8a6f2d721f21db629aa289a3c3c56d00e6677bc3
KrammyGod <shi.mark10@gmail.com>
2023-09-21 15:46:34 -0400

    Add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Twitter Scraper
+A very simple and basic scraping script that will retrieve all raw image links from a specific tweet.
+
+This project is mostly for personal purposes, but feel free to use it if you want to.
+
+## Use instructions:
+1. Clone this repository
+2. Install requirements: `npm i`
+3. Get your twitter token (look for `auth_token` in cookies) and put it in the `.env` file under `TWITTER_AUTH_TOKEN` (see `.env.example`).
+4. Run `npx playwright install` to install the browser binaries.
+5. Run `node index.js` to start the server, or you can simply use `scraper.js` as a module.
---
+++ scraper.js
@@ -0,0 +1,75 @@
+require('dotenv/config');
+const { chromium } = require('playwright');
+
+/**
+ * @type {import('playwright').Browser}
+ */
+let browser = undefined;
+/**
+ * @type {import('playwright').BrowserContext}
+ */
+let context = undefined;
+/**
+ * Must be called before calling getImageUrl.
+ * @returns {Promise<void>}
+ */
+exports.start = async () => {
+    browser = await chromium.launch();
+    context = await browser.newContext();
+    await context.addCookies([{
+        name: 'auth_token',
+        'value' : process.env.TWITTER_AUTH_TOKEN,
+        domain: '.twitter.com',
+        path: '/'
+    }]);
+};
+
+/**
+ * Must be called before closing server.
+ * @returns {Promise<void>}
+ */
+exports.end = async () => {
+    await context.close();
+    await browser.close();
+};
+
+/**
+ * Actual meat of this project.
+ * @param {string} url
+ * @returns {Promise<string[]>}
+ */
+exports.getImageUrl = async (url) => {
+    const page = await context.newPage();
+    await page.goto(url);
+    // Wait for the main photo to load.
+    await page.waitForSelector('div[data-testid="tweetPhoto"]');
+    // Check how many photos there are, and click on first one
+    // The first cellInnerDiv is the original tweet, the rest are replies
+    const tweetPhotos = page.getByTestId('cellInnerDiv').first().locator('a').filter({ has: page.getByTestId('tweetPhoto') });
+    await tweetPhotos.first().click();
+    // Keep scrolling through all the photos to load them all
+    let nav = page.getByLabel('Next slide');
+    // Wait for the navigation button to appear if there is one.
+    // Timeout after 2 seconds (single photo)
+    const canNav = await nav.waitFor({ timeout: 2000 }).then(() => true).catch(() => false);
+    const urls = [];
+    if (canNav) {
+        while (await nav.count()) {
+            await nav.click();
+            nav = page.getByLabel('Next slide');
+        }
+        // Now that all the photos are loaded, we can grab them all at once.
+        const multiplePhotos = page.locator('ul[role="list"] img');
+        const count = await multiplePhotos.count();
+        for (let i = 0; i < count; ++i) {
+            const res = await multiplePhotos.nth(i).getAttribute('src');
+            urls.push(res);
+        }
+    } else {
+        // Single photo is simple.
+        const res = await page.getByRole('img', { name: 'Image' }).first().getAttribute('src');
+        urls.push(res);
+    }
+    await page.close();
+    return urls;
+}
