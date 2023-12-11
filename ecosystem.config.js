9d106aa6e303bfc5a3b03bb9a178ef1114528cf5
KrammyGod <shi.mark10@gmail.com>
2023-12-11 14:29:00 -0500

    Improve deployment flow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,8 @@ module.exports = {
       'ref': 'origin/main',
       'repo': 'git@github.com:KrammyGod/twitter-scraper.git',
       'path': process.env.DEPLOY_PATH,
-      'post-deploy': 'npm ci --omit dev && pm2 start --env production --update-env'
+      'pre-deploy': 'npm ci --omit dev',
+      'post-deploy': 'pm2 start --env production --update-env'
     }
   }
 };
